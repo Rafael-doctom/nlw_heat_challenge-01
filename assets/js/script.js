7949aa0d5fabf46517b8e5459eddcb1d1fb40758
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-04-04 10:29:20 -0300

    feat: update all data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 const SocialObj = {
-    youtube: 'Rafael-doctom',
-    facebook: 'rafaelFake',
-    instagram: 'insFace'
+    github: 'Rafael-doctom',
 }
 
 const ChangeLinks = () => {
@@ -12,3 +10,22 @@ const ChangeLinks = () => {
 }
 
 ChangeLinks()
+
+function getGitHubProfileInfos() {
+    const URL = `https://api.github.com/users/${SocialObj.github}`
+
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+
+            document.getElementById("userName").textContent = `${data.name}`;
+            document.getElementById("profilegithub").textContent = `${data.login}`;
+            document.getElementById("bioGithub").textContent = `${data.bio}`;
+            profilegithub.href = `${data.html_url}`;
+            document.getElementById("imageProfile").textContent = `${data.bio}`;
+            imageProfile.src = `${data.avatar_url}`;
+        })
+}
+
+getGitHubProfileInfos()
